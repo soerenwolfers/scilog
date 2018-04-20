063d49d42eaa559309fdc9d7b1edaaae99ac6fbb
soerenwolfers <soeren.wolfers@gmail.com>
2018-04-20 11:58:12 +0300

    Version 1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scilog',
-    version='1.1.1',
+    version='1.2',
     python_requires='>=3',
     long_description=open('README.rst').read(),
     description='Keep track of numerical experiments',
